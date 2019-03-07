98ac9b014529c5df5888f3a003ff8c754c92917a
tomascelestino <48045657+tomascelestino@users.noreply.github.com>
2019-03-07 10:03:34 +0200

    Calculadora

---
+++ CalculadoraTomas/CalculadoraTomas/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.botao7 = New System.Windows.Forms.Button()
+        Me.botao8 = New System.Windows.Forms.Button()
+        Me.botao9 = New System.Windows.Forms.Button()
+        Me.btnMulti = New System.Windows.Forms.Button()
+        Me.botao4 = New System.Windows.Forms.Button()
+        Me.botao5 = New System.Windows.Forms.Button()
+        Me.botao6 = New System.Windows.Forms.Button()
+        Me.btnDivisao = New System.Windows.Forms.Button()
+        Me.botao3 = New System.Windows.Forms.Button()
+        Me.botao2 = New System.Windows.Forms.Button()
+        Me.botao1 = New System.Windows.Forms.Button()
+        Me.btnMenos = New System.Windows.Forms.Button()
+        Me.btnMais = New System.Windows.Forms.Button()
+        Me.botaoDot = New System.Windows.Forms.Button()
+        Me.botao0 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'botao7
+        '
+        Me.botao7.Location = New System.Drawing.Point(12, 74)
+        Me.botao7.Name = "botao7"
+        Me.botao7.Size = New System.Drawing.Size(45, 23)
+        Me.botao7.TabIndex = 0
+        Me.botao7.Text = "7"
+        Me.botao7.UseVisualStyleBackColor = True
+        '
+        'botao8
+        '
+        Me.botao8.Location = New System.Drawing.Point(62, 74)
+        Me.botao8.Name = "botao8"
+        Me.botao8.Size = New System.Drawing.Size(45, 23)
+        Me.botao8.TabIndex = 1
+        Me.botao8.Text = "8"
+        Me.botao8.UseVisualStyleBackColor = True
+        '
+        'botao9
+        '
+        Me.botao9.Location = New System.Drawing.Point(113, 74)
+        Me.botao9.Name = "botao9"
+        Me.botao9.Size = New System.Drawing.Size(45, 23)
+        Me.botao9.TabIndex = 2
+        Me.botao9.Text = "9"
+        Me.botao9.UseVisualStyleBackColor = True
+        '
+        'btnMulti
+        '
+        Me.btnMulti.Location = New System.Drawing.Point(164, 74)
+        Me.btnMulti.Name = "btnMulti"
+        Me.btnMulti.Size = New System.Drawing.Size(45, 23)
+        Me.btnMulti.TabIndex = 3
+        Me.btnMulti.Text = "*"
+        Me.btnMulti.UseVisualStyleBackColor = True
+        '
+        'botao4
+        '
+        Me.botao4.Location = New System.Drawing.Point(12, 103)
+        Me.botao4.Name = "botao4"
+        Me.botao4.Size = New System.Drawing.Size(45, 23)
+        Me.botao4.TabIndex = 4
+        Me.botao4.Text = "4"
+        Me.botao4.UseVisualStyleBackColor = True
+        '
+        'botao5
+        '
+        Me.botao5.Location = New System.Drawing.Point(62, 103)
+        Me.botao5.Name = "botao5"
+        Me.botao5.Size = New System.Drawing.Size(45, 23)
+        Me.botao5.TabIndex = 5
+        Me.botao5.Text = "5"
+        Me.botao5.UseVisualStyleBackColor = True
+        '
+        'botao6
+        '
+        Me.botao6.Location = New System.Drawing.Point(113, 103)
+        Me.botao6.Name = "botao6"
+        Me.botao6.Size = New System.Drawing.Size(45, 23)
+        Me.botao6.TabIndex = 6
+        Me.botao6.Text = "6"
+        Me.botao6.UseVisualStyleBackColor = True
+        '
+        'btnDivisao
+        '
+        Me.btnDivisao.Location = New System.Drawing.Point(164, 103)
+        Me.btnDivisao.Name = "btnDivisao"
+        Me.btnDivisao.Size = New System.Drawing.Size(45, 23)
+        Me.btnDivisao.TabIndex = 7
+        Me.btnDivisao.Text = "/"
+        Me.btnDivisao.UseVisualStyleBackColor = True
+        '
+        'botao3
+        '
+        Me.botao3.Location = New System.Drawing.Point(12, 132)
+        Me.botao3.Name = "botao3"
+        Me.botao3.Size = New System.Drawing.Size(45, 23)
+        Me.botao3.TabIndex = 8
+        Me.botao3.Text = "3"
+        Me.botao3.UseVisualStyleBackColor = True
+        '
+        'botao2
+        '
+        Me.botao2.Location = New System.Drawing.Point(62, 132)
+        Me.botao2.Name = "botao2"
+        Me.botao2.Size = New System.Drawing.Size(45, 23)
+        Me.botao2.TabIndex = 9
+        Me.botao2.Text = "2"
+        Me.botao2.UseVisualStyleBackColor = True
+        '
+        'botao1
+        '
+        Me.botao1.Location = New System.Drawing.Point(113, 132)
+        Me.botao1.Name = "botao1"
+        Me.botao1.Size = New System.Drawing.Size(45, 23)
+        Me.botao1.TabIndex = 10
+        Me.botao1.Text = "1"
+        Me.botao1.UseVisualStyleBackColor = True
+        '
+        'btnMenos
+        '
+        Me.btnMenos.Location = New System.Drawing.Point(164, 132)
+        Me.btnMenos.Name = "btnMenos"
+        Me.btnMenos.Size = New System.Drawing.Size(45, 23)
+        Me.btnMenos.TabIndex = 11
+        Me.btnMenos.Text = "-"
+        Me.btnMenos.UseVisualStyleBackColor = True
+        '
+        'btnMais
+        '
+        Me.btnMais.Location = New System.Drawing.Point(164, 161)
+        Me.btnMais.Name = "btnMais"
+        Me.btnMais.Size = New System.Drawing.Size(45, 23)
+        Me.btnMais.TabIndex = 15
+        Me.btnMais.Text = "+"
+        Me.btnMais.UseVisualStyleBackColor = True
+        '
+        'botaoDot
+        '
+        Me.botaoDot.Location = New System.Drawing.Point(113, 161)
+        Me.botaoDot.Name = "botaoDot"
+        Me.botaoDot.Size = New System.Drawing.Size(45, 23)
+        Me.botaoDot.TabIndex = 14
+        Me.botaoDot.Text = "."
+        Me.botaoDot.UseVisualStyleBackColor = True
+        '
+        'botao0
+        '
+        Me.botao0.Location = New System.Drawing.Point(12, 161)
+        Me.botao0.Name = "botao0"
+        Me.botao0.Size = New System.Drawing.Size(95, 23)
+        Me.botao0.TabIndex = 13
+        Me.botao0.Text = "0"
+        Me.botao0.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(12, 190)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(95, 23)
+        Me.Button16.TabIndex = 12
+        Me.Button16.Text = "Limpar"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Location = New System.Drawing.Point(113, 190)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(96, 23)
+        Me.btnIgual.TabIndex = 16
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(197, 20)
+        Me.TextBox1.TabIndex = 17
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(145, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Resultado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(221, 219)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnMais)
+        Me.Controls.Add(Me.botaoDot)
+        Me.Controls.Add(Me.botao0)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.btnMenos)
+        Me.Controls.Add(Me.botao1)
+        Me.Controls.Add(Me.botao2)
+        Me.Controls.Add(Me.botao3)
+        Me.Controls.Add(Me.btnDivisao)
+        Me.Controls.Add(Me.botao6)
+        Me.Controls.Add(Me.botao5)
+        Me.Controls.Add(Me.botao4)
+        Me.Controls.Add(Me.btnMulti)
+        Me.Controls.Add(Me.botao9)
+        Me.Controls.Add(Me.botao8)
+        Me.Controls.Add(Me.botao7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents botao7 As Button
+    Friend WithEvents botao8 As Button
+    Friend WithEvents botao9 As Button
+    Friend WithEvents btnMulti As Button
+    Friend WithEvents botao4 As Button
+    Friend WithEvents botao5 As Button
+    Friend WithEvents botao6 As Button
+    Friend WithEvents btnDivisao As Button
+    Friend WithEvents botao3 As Button
+    Friend WithEvents botao2 As Button
+    Friend WithEvents botao1 As Button
+    Friend WithEvents btnMenos As Button
+    Friend WithEvents btnMais As Button
+    Friend WithEvents botaoDot As Button
+    Friend WithEvents botao0 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ CalculadoraTomas/CalculadoraTomas/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+
+    Private Sub botao4_Click(sender As Object, e As EventArgs) Handles botao4.Click
+        Dim botao4 As Button = CType(sender, Button)
+        If botao4.Name = "botao4" Then
+            TextBox1.Text = TextBox1.Text + "4"
+        End If
+    End Sub
+
+    Private Sub botao2_Click(sender As Object, e As EventArgs) Handles botao2.Click
+        Dim botao2 As Button = CType(sender, Button)
+        If botao2.Name = "botao2" Then
+            TextBox1.Text = TextBox1.Text + "2"
+        End If
+    End Sub
+
+    Private Sub botao3_Click(sender As Object, e As EventArgs) Handles botao3.Click
+        Dim botao3 As Button = CType(sender, Button)
+        If botao3.Name = "botao3" Then
+            TextBox1.Text = TextBox1.Text + "3"
+        End If
+    End Sub
+
+    Private Sub botao6_Click(sender As Object, e As EventArgs) Handles botao6.Click
+        Dim botao6 As Button = CType(sender, Button)
+        If botao6.Name = "botao6" Then
+            TextBox1.Text = TextBox1.Text + "6"
+        End If
+    End Sub
+
+    Private Sub botao5_Click(sender As Object, e As EventArgs) Handles botao5.Click
+        Dim botao5 As Button = CType(sender, Button)
+        If botao5.Name = "botao5" Then
+            TextBox1.Text = TextBox1.Text + "5"
+        End If
+    End Sub
+
+    Private Sub botao9_Click(sender As Object, e As EventArgs) Handles botao9.Click
+        Dim botao9 As Button = CType(sender, Button)
+        If botao9.Name = "botao9" Then
+            TextBox1.Text = TextBox1.Text + "9"
+        End If
+    End Sub
+
+    Private Sub botao8_Click(sender As Object, e As EventArgs) Handles botao8.Click
+        Dim botao8 As Button = CType(sender, Button)
+        If botao8.Name = "botao8" Then
+            TextBox1.Text = TextBox1.Text + "8"
+        End If
+    End Sub
+
+    Private Sub botao7_Click(sender As Object, e As EventArgs) Handles botao7.Click
+        Dim botao7 As Button = CType(sender, Button)
+        If botao7.Name = "botao7" Then
+            TextBox1.Text = TextBox1.Text + "7"
+        End If
+    End Sub
+
+    Private Sub botao0_Click(sender As Object, e As EventArgs) Handles botao0.Click
+        Dim botao0 As Button = CType(sender, Button)
+        If botao0.Name = "botao0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+    End Sub
+
+    Private Sub botao1_Click(sender As Object, e As EventArgs) Handles botao1.Click
+        Dim botao1 As Button = CType(sender, Button)
+        If botao1.Name = "botao1" Then
+            TextBox1.Text = TextBox1.Text + "1"
+        End If
+    End Sub
+
+    Private Sub btnMais_Click(sender As Object, e As EventArgs) Handles btnMais.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnMais" Then
+            TextBox1.Text = TextBox1.Text + "-"
+        End If
+    End Sub
+
+    Private Sub btnMenos_Click(sender As Object, e As EventArgs) Handles btnMenos.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnMenos" Then
+            TextBox1.Text = TextBox1.Text + "-"
+        End If
+    End Sub
+
+    Private Sub btnDivisao_Click(sender As Object, e As EventArgs) Handles btnDivisao.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnDivisao" Then
+            TextBox1.Text = TextBox1.Text + "/"
+        End If
+    End Sub
+
+    Private Sub btnMulti_Click(sender As Object, e As EventArgs) Handles btnMulti.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnMulti" Then
+            TextBox1.Text = TextBox1.Text + "*"
+        End If
+    End Sub
+
+    Private Sub botaoDot_Click(sender As Object, e As EventArgs) Handles botaoDot.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnDot" Then
+            TextBox1.Text = TextBox1.Text + "."
+        End If
+    End Sub
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "btnIgual" Then
+            Dim equation As String = TextBox1.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox1.Text = result
+        End If
+
+        If button.Name = "btnDivisao" Then
+            TextBox1.Text = TextBox1.Text + "/"
+        End If
+
+        If button.Name = "btnMais" Then
+            TextBox1.Text = TextBox1.Text + "+"
+        End If
+        If button.Name = "btnMenos" Then
+            TextBox1.Text = TextBox1.Text + "-"
+        End If
+        If button.Name = "ButtonCLR" Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+End Class
